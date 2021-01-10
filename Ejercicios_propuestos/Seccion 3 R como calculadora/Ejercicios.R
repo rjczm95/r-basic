e9a66f4932b0dc5a29f5baa6c9c8553358ce82eb
rjczm95 <ramonjesusceballos@gmail.com>
2021-01-10 17:35:28 +0100

    10/01/2021

---
+++ Ejercicios_propuestos/Seccion 3 R como calculadora/Ejercicios.R
@@ -0,0 +1,56 @@
+#Ejercicio 1
+
+seg=250000000
+horas=seg/3600
+
+dias=horas/24
+dias
+
+año_2018=365
+año_2019=365
+año_2020=366
+año_2021=365
+año_2022=365
+año_2023=365
+año_2024=366
+año_2025=365
+
+dias_tras_2018=dias-año_2018
+dias_tras_2019=dias_tras_2018-año_2019
+dias_tras_2020=dias_tras_2019-año_2020
+dias_tras_2021=dias_tras_2020-año_2021
+dias_tras_2022=dias_tras_2021-año_2022
+dias_tras_2023=dias_tras_2022-año_2023
+dias_tras_2024=dias_tras_2023-año_2024
+dias_tras_2024
+
+tras01=dias_tras_2024-31
+sum=28+31+30+31+30+31+31+30+31
+
+meses=tras01-sum-30-2
+horas=meses*24
+min=(horas-12)*60
+min
+#3 de diciembre del año 2025 a las 12:26
+
+
+#Ejercicio 2
+
+función_ej2=function(A,B,C){(C-B)/A}
+función_ej2(1,1,1)
+
+#Ejercicio 3
+
+expresion1=3*exp(1)-pi
+round(expresion1,3)
+
+expresion2=(2+3i)^2/(5+8i)
+round(Mod(expresion2),3)
+
+
+
+
+
+
+
+
--- scripts/tema1/04-calculadora.R
+++ scripts/tema1/04-calculadora.R
@@ -221,14 +221,14 @@ z1
 sqrt(-5)
 sqrt(as.complex(-5))
 
-#La raíz cuadrada devuelve, de las dos soluciones la de
+#La raiz cuadrada devuelve, de las dos soluciones la de
 #Re(z)>0, para obtener la otra,  hay que multiplicar por -1
 sqrt(3+2i) # z^2 = 3+2i
 exp(3+2i)
 sin(3+2i)
 cos(3+2i)
 
-#Módulo = sqrt(Re(z)^2 + Im(z)^2)
+#Modulo = sqrt(Re(z)^2 + Im(z)^2)
 Mod(z1)
 #Argumento = arctan(Im(z)/Re(z))
 # = arccos(Re(z)/Mod(z))
